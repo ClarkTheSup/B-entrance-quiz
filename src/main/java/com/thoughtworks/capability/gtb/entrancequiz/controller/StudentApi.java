b54a9beacc95d352d59904558927eb42c5db31ec
ClarkTheSup <lknkoqq123@163.com>
2020-09-07 15:13:20 +0800

    [kangning] feat: cross origin

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentApi.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentApi.java
@@ -4,6 +4,7 @@ import com.thoughtworks.capability.gtb.entrancequiz.domain.Student;
 import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class StudentApi {
 
     @GetMapping("/students")
     @ResponseStatus(HttpStatus.OK)
+    @CrossOrigin
     public List<Student> getStudentList() {
         return studentService.getStudentList();
     }
