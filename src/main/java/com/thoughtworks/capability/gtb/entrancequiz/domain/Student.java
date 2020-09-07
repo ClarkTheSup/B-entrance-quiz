b8f96e28ae9de70a859686297f0299d8160badc8
ClarkTheSup <lknkoqq123@163.com>
2020-09-07 15:53:34 +0800

    [kangning] feat: add student singleton factory

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/domain/Student.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/domain/Student.java
@@ -1,10 +1,20 @@
 package com.thoughtworks.capability.gtb.entrancequiz.domain;
 
 public class Student {
+    private int id;
     private String name;
 
-    public Student(String name) {
+    public Student(int id, String name) {
+        this.id = id;
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
