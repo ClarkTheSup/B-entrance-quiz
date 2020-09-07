d6db74807a264dbde63ada54a7ba1900a2d60706
ClarkTheSup <lknkoqq123@163.com>
2020-09-07 15:01:33 +0800

    [kangning] feat: add StudentApi

---
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentApi.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.capability.gtb.entrancequiz.controller;
+
+import com.thoughtworks.capability.gtb.entrancequiz.domain.Student;
+import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentApi {
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping("/students")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Student> getStudentList() {
+        return studentService.getStudentList();
+    }
+}
